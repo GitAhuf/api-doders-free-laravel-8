57d853a814f70cc815c5cdbacd14ec7722876b0c
ahuf <recibosalvaro@gmail.com>
2023-05-31 22:26:02 -0500

    Ordenar recursos

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -13,6 +13,7 @@ class Category extends Model
 
     protected $allowIncluded = ['posts', 'posts.user'];
     protected $allowFilter = ['id', 'name', 'slug'];
+    protected $allowSort = ['id', 'name', 'slug'];
 
     //Relacion uno a muchos
     public function posts(){
@@ -52,4 +53,28 @@ class Category extends Model
             }
         }
     }    
+
+    public function scopeSort(Builder $query){
+
+        if(empty($this->allowSort) || empty(request('sort'))){
+            return;
+        }
+
+        $sortFields = explode(',', request('sort'));
+        $allowSort = collect($this->allowSort);
+
+        foreach($sortFields as $sortField){
+
+            $direction = 'asc';
+
+            if(substr($sortField, 0, 1) == '-'){
+                $direction = 'desc';
+                $sortField = substr($sortField, 1);
+            }
+
+            if($allowSort->contains($sortField)){
+                $query->orderBy($sortField, $direction);
+            }
+        }
+    }
 }
